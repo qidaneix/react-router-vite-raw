703ef2e386980c2952095c180bbe34fcc8739381
jin.xiao <jin.xiao@shopee.com>
2022-08-04 11:26:31 +0800

    feat: update

--- src/utils/CacheRoutes.tsx
+++ src/utils/CacheRoutes.tsx
@@ -3,12 +3,17 @@ import {
   createRoutesFromChildren,
   RouteObject,
   RoutesProps,
+  matchRoutes,
+  useLocation,
+  UNSAFE_NavigationContext,
+  UNSAFE_RouteContext,
 } from "react-router-dom";
 import { Location } from "history";
 import { useCreateCache } from "./CreateCache";
 import React from "react";
 
 interface CacheRouteObject extends RouteObject {
+  children?: CacheRouteObject[];
   cache?: boolean;
 }
 
@@ -17,7 +22,11 @@ export function useCacheRoutes(
   locationArg?: Partial<Location> | string
 ): React.ReactElement | null {
   const currentOutlet = useRoutes(routes, locationArg);
-
+  const location = useLocation();
+  const { basename } = React.useContext(UNSAFE_NavigationContext);
+  const RouteContext = React.useContext(UNSAFE_RouteContext);
+  console.log("matchRoutes", matchRoutes(routes, location, basename));
+  console.log("RouteContext", RouteContext);
   return useCreateCache(currentOutlet);
 }
 
